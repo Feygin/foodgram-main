f6d3c29da58ee2675ac19e6f9177c5fda6e0b18a
Alexander Feygin <A.Feygin@alfabank.ru>
2025-11-17 23:55:10 +0300

    Added settings to run project locally

---
+++ backend/foodgram/settings_local.py
@@ -0,0 +1,17 @@
+from .settings import *
+
+# ————————————————————————————
+# ЛОКАЛЬНАЯ БАЗА ДЛЯ РАЗРАБОТКИ (SQLite)
+# ————————————————————————————
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Включаем DEBUG локально
+DEBUG = True
+
+# Разрешаем localhost
+ALLOWED_HOSTS = ['*']

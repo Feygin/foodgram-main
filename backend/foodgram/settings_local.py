d5bdd48511d2306de701a350521cbd3d0ce00298
Alexander Feygin <A.Feygin@alfabank.ru>
2025-11-22 21:09:09 +0300

    Added debug toolbar

--- backend/foodgram/settings_local.py
+++ backend/foodgram/settings_local.py
@@ -15,3 +15,10 @@ DEBUG = True
 
 # Разрешаем localhost
 ALLOWED_HOSTS = ["*"]
+
+# Добавляем django_debug_toolbar
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
